d65524059247941740f0dcda6b23a38f3723a29a
Ojbilli <onem08989@gmail.com>
2023-07-03 00:40:43 +0100

    new

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require ('express');
 
 
-const friendsController = require('./controllers/friends.controller');
-const messagesController = require('./controllers/messages.controller');
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
@@ -13,18 +13,16 @@ app.use((req, res, next) => {
     const start = Date.now();
     next();
     const delta = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${delta}ms`);
+    console.log(`${req.method} ${req.baseUrl} ${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.post('/friend', friendsController.postFriend );
-app.get('/friends', friendsController.getFriends);
-app.get('/friends/:friendId',friendsController.getFriend );
 
 
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessages);
+app.use('/friends',friendsRouter);
+app.use('/messages',messagesRouter);
+
 
 app.listen(PORT, ()=> {
     console.log (`listening on ${PORT}....`);
